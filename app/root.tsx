d46f66adb18d27866c57b08661f43740ae22d851
Jonas Gonzalez <jonasok82@gmail.com>
2025-12-09 16:00:36 -0400

    Update root with fixed loader

--- app/root.tsx
+++ app/root.tsx
@@ -4,16 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  return {
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  };
+}
+
 export default function App() {
+  const { apiKey } = useLoaderData<typeof loader>();
+
   return (
     <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="shopify-api-key" content={apiKey} />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js" />
         <Meta />
         <Links />
       </head>
