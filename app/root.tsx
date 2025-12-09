ee5602fd0e33f7c3ff9c2c249d633d68f75c69dd
Jonas Gonzalez <jonasok82@gmail.com>
2025-12-09 15:46:08 -0400

    Fix TypeScript configuration

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,19 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/node";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  
-  return json({ 
-    apiKey: process.env.SHOPIFY_API_KEY || "",
-    host: url.searchParams.get("host") || "",
-  });
-}
-
 export default function App() {
-  const { apiKey, host } = useLoaderData<typeof loader>();
-
   return (
     <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="shopify-api-key" content={apiKey} />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
