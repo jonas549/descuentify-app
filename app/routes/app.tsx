634864eb16b66d48ce02315b4f8a6b451d64374e
Jonas Gonzalez <jonasok82@gmail.com>
2025-12-10 09:12:25 -0400

    prueba claude6

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,13 +1,24 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { AppProvider } from "@shopify/polaris";
 import { authenticate } from "../utils/shopify.server";
 import { AppLayout } from "../components/layout/AppLayout";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await authenticate.admin(request);
-  return json({});
+  
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  });
 }
 
 export default function AppLayoutRoute() {
-  return <AppLayout />;
+  const { apiKey } = useLoaderData<typeof loader>();
+  
+  return (
+    <AppProvider i18n={{}}>
+      <AppLayout />
+    </AppProvider>
+  );
 }
